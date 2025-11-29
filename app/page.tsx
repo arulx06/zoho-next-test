fdd7af1e2d9704eb5b0fed0e3b7ba713b25aaebe
ArulMozhiVarman <arulx06@gmail.com>
2025-11-29 20:34:36 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 "use client";
 
 export default function Home() {
-  const params = new URLSearchParams(window.location.search);
-  const zoho = params.get("zoho");
+  let zohoData = null;
+
+  if (typeof window !== "undefined") {
+    const params = new URLSearchParams(window.location.search);
+    const zoho = params.get("zoho");
+
+    if (zoho) {
+      try {
+        zohoData = JSON.parse(decodeURIComponent(zoho));
+      } catch (err) {
+        console.error("Failed to parse Zoho data", err);
+      }
+    }
+  }
 
   return (
-    <div>
+    <div style={{ padding: 20 }}>
       <h1>Zoho Next Test</h1>
-      <pre>{zoho ? JSON.stringify(JSON.parse(zoho), null, 2) : "No Zoho data"}</pre>
+      <pre>
+        {zohoData
+          ? JSON.stringify(zohoData, null, 2)
+          : "No Zoho data received"}
+      </pre>
     </div>
   );
 }
